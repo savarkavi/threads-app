555b9eb9edb035f25d8b3a501a4803e8ac75e3be
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-05-12 07:42:43 +0530

    updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://threads-app-uhbt.onrender.com/",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
