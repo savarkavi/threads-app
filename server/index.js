6ac0b4ce4742904b4fdff5c820764b21ae3f48f6
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-05-12 07:37:20 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import postRoutes from "./routes/post.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
