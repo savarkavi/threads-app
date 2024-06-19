ed52d6ca66023ae40c26c850cecace837d45ab3c
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-06-19 09:19:35 +0530

    updated

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "/server/public/uploads");
+    cb(null, "../public/uploads");
   },
   filename: (req, file, cb) => {
     const fileName = file.originalname;
